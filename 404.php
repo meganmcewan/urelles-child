4d1b7fb2d9aa6c59c4de59577cd33b10bcb7bcc1
Megan McEwan <mlmcewan@gmail.com>
2019-04-30 19:54:59 -0400

    more tests

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@
 	<div class="container">
 		<div class="container_inner q_404_page default_template_holder">
 			<div class="page_not_found">
-			goodbye
+			goodbye!!!!
 				<h2><?php if($qode_options['404_title'] != ""): echo esc_html($qode_options['404_title']); else: ?> <?php _e('Page you are looking is Not Found', 'qode'); ?> <?php endif;?></h2>
 
                 <h4><?php if($qode_options['404_text'] != ""): echo esc_html($qode_options['404_text']); else: ?> <?php _e('The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site’s homepage and see if you can find what you are looking for.', 'qode'); ?> <?php endif;?></h4>
