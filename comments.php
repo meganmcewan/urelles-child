1aef86aee614dcfcbef79fdde6d80c3471d32d2d
Megan McEwan <mlmcewan@gmail.com>
2019-05-04 18:45:27 -0400

    comment translations

---
+++ comments.php
@@ -0,0 +1,65 @@
+<div class="comment_holder clearfix" id="comments">
+<div class="comment_number"><div class="comment_number_inner"><h5><?php comments_number( __('','qode'), '1'.__(' Comment ','qode'), '% '.__(' Comments ','qode')); ?></h5></div></div>
+<div class="comments">
+<?php if ( post_password_required() ) : ?>
+				<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'qode' ); ?></p>
+			</div></div>
+<?php
+		
+		return;
+	endif;
+?>
+<?php if ( have_comments() ) : ?>
+
+	<ul class="comment-list">
+		<?php wp_list_comments(array( 'callback' => 'qode_comment')); ?>
+	</ul>
+
+
+<?php // End Comments ?>
+
+ <?php else : // this is displayed if there are no comments so far 
+
+	if ( ! comments_open() ) :
+?>
+		<!-- If comments are open, but there are no comments. -->
+
+	 
+		<!-- If comments are closed. -->
+	
+
+	<?php endif; ?>
+<?php endif; ?>
+</div></div>
+<?php
+$commenter = wp_get_current_commenter();
+$req = get_option( 'require_name_email' );
+$aria_req = ( $req ? " aria-required='true'" : '' );
+
+$args = array(
+	'id_form' => 'commentform',
+	'id_submit' => 'submit_comment',
+	'title_reply'=>'<h5>'. __( 'Poster un commentaire','qode' ) .'</h5>',
+	'title_reply_to' => __( 'Poster un réponse','qode' ),
+	'cancel_reply_link' => __( 'Annuler la réponse','qode' ),
+	'label_submit' => __( 'Soumettre','qode' ),
+	'comment_field' => '<textarea id="comment" placeholder="'.__( 'Écrivez votre commentaire ici...','qode' ).'" name="comment" cols="45" rows="8" aria-required="true"></textarea>',
+	'comment_notes_before' => '',
+	'comment_notes_after' => '',
+	'fields' => apply_filters( 'comment_form_default_fields', array(
+		'author' => '<div class="three_columns clearfix"><div class="column1"><div class="column_inner"><input id="author" name="author" placeholder="'. __( 'Votre nom complet','qode' ) .'" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '"' . $aria_req . ' /></div></div>',
+		'url' => '<div class="column2"><div class="column_inner"><input id="email" name="email" placeholder="'. __( 'Courriel','qode' ) .'" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '"' . $aria_req . ' /></div></div>',
+		'email' => '<div class="column3"><div class="column_inner"><input id="url" name="url" type="text" placeholder="'. __( 'Site web','qode' ) .'" value="' . esc_attr( $commenter['comment_author_url'] ) . '" /></div></div></div>'
+		 ) ) );
+ ?>
+ <div class="comment_pager">
+	<p><?php paginate_comments_links(); ?></p>
+ </div>
+ <div class="comment_form">
+	<?php comment_form($args); ?>
+</div>
+						
+								
+							
+
+
