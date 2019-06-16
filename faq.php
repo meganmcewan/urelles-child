e7aaeb848377357239e0394269caff7f89f4b276
Megan McEwan <mlmcewan@gmail.com>
2019-06-15 20:07:45 -0400

    faq styles and functionality

--- faq.php
+++ faq.php
@@ -6,33 +6,24 @@ Template Name: FAQ
 
 <?php get_header(); ?>
 
-
-<?php if ( has_post_thumbnail() ): ?>
-    <?php the_post_thumbnail(); ?>
-<?php endif; ?>
-    
-    
-
-<div class="container">
-    <div class="container_inner default_template_holder ">
-
-    <?php the_content(); ?>
-    <div class="faq-single">
-        <span class="plus-button">hello</a>
-   
-
-
-    <div></div>
-
-</div>
-
-
+<div class="page-faq">
+    <div class="featured-image">
+    <?php if ( has_post_thumbnail() ): ?>
+        <?php the_post_thumbnail(); ?>
+    <?php endif; ?>
     </div>
+        
+        
 
-   
+ 
+    <div id="custom-container" class="container-custom">
+       
+
+        <?php the_content(); ?>
+    
+        </div>
 
 </div>
-
 
 
 <?php get_footer(); ?>
--- js/urelles_js.js
+++ js/urelles_js.js
@@ -4,9 +4,16 @@ var $j = jQuery.noConflict();
 $j(document).ready(function() {
     "use strict";
     
+$j('.plus-btn').click(function(){
+
+    $j(event.target).closest('.faq-single').find('.faq-answer').toggleClass('open-faq')
+    $j(event.target).closest('.faq-single').find('.plus-btn').toggleClass('open-faq')
+    
 
 
-    console.log('hellooooo')
+})
+
+    
 
     
 
