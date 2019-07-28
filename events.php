336cf5630bfb100ec3bc946351a574246a0ab0bc
Megan McEwan <mlmcewan@gmail.com>
2019-07-28 16:09:41 -0400

    set up events template, add cta, and style all events page icluding responsive

--- events.php
+++ events.php
@@ -14,35 +14,18 @@ Template Name: Events
     </div>
     
     <div class="events-container">
-        <h1 class="events-title">Events</h1>
-        <h3>Stay up to date with events that we're organizing, attenting and just excited about!</h3>
-
-        <div class="month-wrapper">
-            <h2>July</h2>
-            <div class="event-row">
-                <div class="event-single">
-                <div class="event-content">
-                    <p>Get exicted something cool is happening in your neighbourhood</p>
-                    <h3>The big event</h3>
-                    <h6 class="event-date">July 30th</h6>
-                    <a href="#">Register Now</a>
-                    <p>see more</p>
-
-                        
-                        </div>
-                    <div class="event-img">
-                        <img src="https://placebear.com/800/400" alt="">
-                    </div>
-                    
-                </div>
-            </div>
+        <?php the_content(); ?>      
+    </div>
+    <div class="events-cta" >
+        <div class="cta-content">
+           Want to know about upcoming events? Subscribe to our newsletter for all the latest information.
         </div>
-       
-
+        <div class="button-wrap">
+            <a href="/join-our-newsletter/" class='cta-btn'>ABONNEZ-VOUS</a>
+        </div>
+	</div>
         
-    
-        </div>
-        <!-- <?php the_content(); ?> -->
+       
 
 </div>
 
